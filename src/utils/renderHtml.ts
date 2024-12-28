66cc5439ea566c7f6b40cf68bf5ed624b1dc796a
heggland <kjetilheggland@gmail.com>
2024-12-28 13:41:02 +0100

    Update renderHtml.ts

--- src/utils/renderHtml.ts
+++ src/utils/renderHtml.ts
@@ -3,6 +3,10 @@ class RenderHtmlPage {
   private title: string = '';
   private language: string = 'en';
   private description: string = '';
+  private keywords: string[] = [''];
+  private author: string = '';
+  private robots: string = '';
+  private bodyContent: string = '';
 
   constructor(title: string, language: string = 'en') {
     this.title = title;
@@ -13,19 +17,27 @@ class RenderHtmlPage {
     this.description = description;
   }
 
+  // Method to set body content
+  setBody(content: string) {
+    this.bodyContent = content;
+  }
+
   renderLayout(): string {
     return `
             <!DOCTYPE html>
-            <html lang="en">
+            <html lang="${this.language}">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>${this.title}</title>
                 <meta name="description" content="${this.description}">
                 <link rel="stylesheet" href="styles.css">
+                <meta name="keywords" content="${this.keywords.join(',')}">
+                <meta name="author" content="${this.author}">
+                <meta name="robots" content="${this.robots}">
             </head>
             <body>
-            hello world
+            ${this.bodyContent}
             </body>
             </html>
         `;
